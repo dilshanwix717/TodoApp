b90741366085820b9cbcdda8991d6e9efca0c184
Dilshan Wickramasinghe <dilshanmw717@gmail.com>
2024-05-16 17:21:44 +0530

    new modifications

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Http\Request;
+use App\Models\Product;
+
+
 
 class HomeController extends Controller
 {
     public function index()
     {
         return view('pages.home.index');
+    }
+    public function relationship()
+    {
+        $response['products'] = Product::all();
+        return view('pages.relationship.index')->with($response);
     }
 }
